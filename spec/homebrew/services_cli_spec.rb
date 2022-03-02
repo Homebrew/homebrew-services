94f66de258b5f05eccb24bee68cd4278393cf698
apainintheneck <apainintheneck@gmail.com>
2022-03-01 19:08:16 -0800

    Fixed more syntax errors.

--- spec/homebrew/services_cli_spec.rb
+++ spec/homebrew/services_cli_spec.rb
@@ -54,8 +54,8 @@ describe Service::ServicesCli do
     end
 
     it "checks if target service is already running and suggests restart instead" do
-      expected_output = "Service `example_service` already running," +
-        " use `brew services restart example_service` to restart.\n"
+      expected_output = "Service `example_service` already running," \
+                        " use `brew services restart example_service` to restart.\n"
       service = instance_double("service", name: "example_service", pid?: true)
       expect do
         services_cli.run([service])
@@ -77,8 +77,8 @@ describe Service::ServicesCli do
     end
 
     it "checks if target service has already been started and suggests restart instead" do
-      expected_output = "Service `example_service` already started," +
-        " use `brew services restart example_service` to restart.\n"
+      expected_output = "Service `example_service` already started," \
+                        " use `brew services restart example_service` to restart.\n"
       service = instance_double("service", name: "example_service", pid?: true)
       expect do
         services_cli.start([service])
